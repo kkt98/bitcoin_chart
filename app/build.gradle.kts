640afaac1399946e8ce379c82ace8f4841312194
kkt98 <sux1011@naver.com>
2026-01-15 01:21:31 +0900

    android Jetpack CoinApp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.kkt981019.bitcoin_chart"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
